0c0d723e6f156abfa9aab4d4a5201ade82166d22
murilokauan.1@gmail.com <murilokauan.1@gmail.com>
2023-12-05 16:47:28 -0300

    5.12.4

--- manager/views.py
+++ manager/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Campanha, Anotacao, Personagem
 from .forms import CampanhaForm
 from django.views.generic import ListView,CreateView,DeleteView,DetailView, UpdateView,TemplateView
-from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.contrib.messages import views
 from django.db.models import Q
+from django.http import HttpResponseForbidden
+
 
 class ListarMCampanhas(ListView):
     template_name = "manager/minhas_campanhas.html"
     model = Campanha
     context_object_name = 'campanhas'
     paginate_by='5'
-    
+
     def get_queryset(self):
         search = self.request.GET.get('search')
-
         if search:
             campanhas = Campanha.objects.filter(nome__icontains=search)
         else:
@@ -48,7 +48,7 @@ class CampanhaCreateView(CreateView):
         form = CampanhaForm(user=request.user if request.user.is_authenticated else None)
         return render(request, self.template_name, {'form': form})
 
-class TesteDashboard(TemplateView):
+class Dashboard(TemplateView):
     template_name = "manager/dashboard/dashboard.html"
 
     def get_perm(self):
