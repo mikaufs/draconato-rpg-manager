d6abe24eab0e6ff2da0c21bb1cba31441f33f877
Yasmim L <yasmimlouise2016@outlook.com>
2023-12-05 15:09:03 -0300

    5.12.2

--- manager/views.py
+++ manager/views.py
@@ -12,11 +12,18 @@ class ListarMCampanhas(ListView):
     template_name = "manager/minhas_campanhas.html"
     model = Campanha
     context_object_name = 'campanhas'
+    paginate_by = 4
 
     def get_queryset(self):
-        user = self.request.user
-        queryset = Campanha.objects.filter(Q(mestre=user) | Q(jogador=user))
-        return queryset
+        search = self.request.GET.get('search')
+
+        if search:
+            campanhas = Campanha.objects.filter(nome__icontains=search)
+        else:
+            user = self.request.user
+            campanhas = Campanha.objects.filter(Q(mestre=user) | Q(jogador=user))
+
+        return campanhas
 
 class ListarInicio(LoginRequiredMixin, ListView):
     template_name = "manager/index.html"
@@ -48,4 +55,3 @@ class TesteDashboard(TemplateView):
         
         if user != queryset:
             return reverse_lazy('index')
-# Create your views here.
